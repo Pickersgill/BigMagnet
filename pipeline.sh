0dad550abb1f9f3748f2db9dbf43f994a26d486a
Pickersgill <jack.pp@btopenworld.com>
2024-02-28 14:26:21 +0000

    Updated pipeline script to show total size of extracted files.

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,10 @@
 rm -rf archives/*
-python3 searcher.py -s $1 -l rust -i mit -o ./results/temp.json -m 100000
+python3 searcher.py -s 10 -l rust -i mit -o ./results/temp.json -m 100000
 python3 archive_getter.py -s ./results/temp.json -o ./archives -b 10
 python3 unpacker.py -s archives -q ".*\.rs$" -r
 
 files=$(find archives/ -type f | wc -l)
 lines=$(find archives/ -type f -exec cat {} + | wc -l)
+data=$(du -hs archives/ | awk '{print $1}')
 echo "Found $files files containing $lines lines of Rust code."
+echo "Amounting to $data of data."
